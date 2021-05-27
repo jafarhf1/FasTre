9b8e4a5a1ed04e5de9170d95cee2f1f31c6a186b
Leonardus Kevin <leonardus.kevin@ui.ac.id>
2021-05-27 10:42:36 +0700

    Age and BloodType Added

--- app/src/main/java/com/example/fastre/ui/authentication/User.java
+++ app/src/main/java/com/example/fastre/ui/authentication/User.java
@@ -2,14 +2,17 @@ package com.example.fastre.ui.authentication;
 
 public class User {
 
-    public String fullName, email;
+    public String fullName, age, bloodType, email;
+
 
     public User() {
 
     }
 
-    public User(String fullName, String email) {
+    public User(String fullName, String age, String bloodType, String email) {
         this.fullName = fullName;
+        this.age = age;
+        this.bloodType = bloodType;
         this.email = email;
     }
 
